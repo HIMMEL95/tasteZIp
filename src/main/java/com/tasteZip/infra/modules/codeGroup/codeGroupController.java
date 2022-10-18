7fa96705ffd7b6abfb03475d54a63887be956678
Laelpark <gottkfkd@naver.com>
2022-10-18 14:14:05 +0900

    2022-10-18

---
+++ src/main/java/com/tasteZip/infra/modules/codeGroup/codeGroupController.java
@@ -0,0 +1,14 @@
+package com.tasteZip.infra.modules.codeGroup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/codeGroup/")
+public class codeGroupController {
+
+	@RequestMapping(value = "codeGroupList")
+	public String codeGroupList() throws Exception {
+		return "infra/xdmin/codeGroup/codeGroupList";
+	}
+}
